8a550925609477d3d2cdb070513a3c0f9c91c645
Abdeljalil Sersif <sersif.a366@ucd.ac.ma>
2025-05-07 18:09:04 +0100

    la partie backend deployer

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -78,7 +78,8 @@ function Contact() {
     
     try {
       // Envoyer les données au backend
-      const response = await fetch('http://localhost:5000/api/contact', {
+      const response = await fetch('https://portfolio-3d-production.up.railway.app/api/contact', {
+
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
